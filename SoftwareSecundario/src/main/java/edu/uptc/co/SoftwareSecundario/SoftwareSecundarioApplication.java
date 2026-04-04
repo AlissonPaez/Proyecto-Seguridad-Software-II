746391d560d57c6da3d239bbc4ac86f33db5cdb1
KimberlyFigueroa21 <kimfigueroa210004@gmail.com>
2026-04-03 21:18:47 -0500

    feat: Initialize main project structure with Spring Boot
    - Added pom.xml for project dependencies and configuration.
    - Created DemoApplication as the entry point for the application.
    - Implemented ControladorUsuario for user registration and login functionalities.
    - Defined Usuario model for user data representation.
    - Established RepositorioUsuario for database interactions.
    - Developed ServicioUsuario for user-related business logic.
    - Configured application properties for basic settings.
    - Added unit tests for application context loading.
    - Set up Maven wrapper for consistent build environment.
    - Created .gitignore and .gitattributes for project management.

---
+++ SoftwareSecundario/src/main/java/edu/uptc/co/SoftwareSecundario/SoftwareSecundarioApplication.java
@@ -0,0 +1,13 @@
+package edu.uptc.co.SoftwareSecundario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareSecundarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareSecundarioApplication.class, args);
+	}
+
+}
---
+++ SoftwareSecundario/src/test/java/edu/uptc/co/SoftwareSecundario/SoftwareSecundarioApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uptc.co.SoftwareSecundario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareSecundarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
